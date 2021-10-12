137117f64c6010362a6a0178b118b20716e99717
Amin Latifi <a.latifi.al@gmail.com>
2021-10-12 20:29:46 +0330

    Removed transfer hook from UnipoolTokenDistributor.sol Defined new GardenUnipoolTokenDistributor.sol which has requirements of Garden Unipool and its Withdraw and Stake functions are disabled

---
+++ contracts/Distributors/GardenUnipoolTokenDistributor.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity =0.8.6;
+
+import "./UnipoolTokenDistributor.sol";
+import "./TokenManagerHook.sol";
+
+/**
+ * Contract will have the same functionalieis of UnipoolTokenDistributor, except
+ * direct Withdraw, Stake and StakeWithPermit functions. These functions are disabled
+ * in favor of hooks which will be used by 1Hive Garden
+ */
+
+contract GardenUnipoolTokenDistributor is
+    UnipoolTokenDistributor,
+    TokenManagerHook
+{
+    /**
+     * @dev Overrides TokenManagerHook's `_onTransfer`
+     */
+    function _onTransfer(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) internal override returns (bool) {
+        if (_from == address(0)) {
+            // Token mintings (wrapping tokens)
+            super.stake(_amount, _to);
+            return true;
+        } else if (_to == address(0)) {
+            // Token burning (unwrapping tokens)
+            super.withdraw(_amount, _from);
+            return true;
+        } else {
+            // Standard transfer
+            super.withdraw(_amount, _from);
+            super.stake(_amount, _to);
+            return true;
+        }
+    }
+
+    function stakeWithPermit(uint256 amount, bytes calldata permit)
+        public
+        override
+    {
+        revert("GardenUnipoolTokenDistributor: disabled");
+    }
+
+    function stake(uint256 amount) public override {
+        revert("GardenUnipoolTokenDistributor: disabled");
+    }
+
+    function withdraw(uint256 amount) public override updateReward(msg.sender) {
+        revert("GardenUnipoolTokenDistributor: disabled");
+    }
+}
