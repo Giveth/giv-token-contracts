bd6635a9c8fd89988c122f6ac01ade60225f1d84
Amin Latifi <a.latifi.al@gmail.com>
2021-11-17 20:44:16 +0330

    Fixed linting issues

--- contracts/Mocks/UnipoolTokenDistributorMock.sol
+++ contracts/Mocks/UnipoolTokenDistributorMock.sol
@@ -4,11 +4,14 @@ pragma solidity =0.8.6;
 
 import "../Distributors/UnipoolTokenDistributor.sol";
 
-
 contract UnipoolTokenDistributorMock is UnipoolTokenDistributor {
     uint256 public currentTimestamp;
 
-    constructor(IDistro _tokenDistribution, IERC20Upgradeable _uni, uint256 _duration) {
+    constructor(
+        IDistro _tokenDistribution,
+        IERC20Upgradeable _uni,
+        uint256 _duration
+    ) {
         initialize(_tokenDistribution, _uni, _duration);
     }
 }
