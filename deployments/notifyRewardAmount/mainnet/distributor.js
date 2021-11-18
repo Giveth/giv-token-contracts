b788b76870ae91c71a705ad496ab95279710dff4
Amin Latifi <a.latifi.al@gmail.com>
2021-11-18 16:46:40 +0330

    Update GIV/ETH on xdai to sushiswap

--- deployments/notifyRewardAmount/mainnet/distributor.js
+++ deployments/notifyRewardAmount/mainnet/distributor.js
@@ -4,12 +4,20 @@ const { ethers } = hre;
 
 const pools = [
     // { address: "0x51F3E5C39a11fe189585FA2FD61A6b60E4Bc723a", amount: "1100000" }, // Uniswap V3 is exception, get all at the beginning
-    { address: "0x5dA8196427475C0026B465454156f0D31236C88B", amount: "2500000" }, // BAL
-    { address: "0x61296aEC102bE83Aaa350B408b5F6B9466F86Dd9", amount: "2500000" }, // $GIV
+    {
+        address: "0x5dA8196427475C0026B465454156f0D31236C88B",
+        amount: "2500000",
+    }, // BAL
+    {
+        address: "0x61296aEC102bE83Aaa350B408b5F6B9466F86Dd9",
+        amount: "2500000",
+    }, // $GIV
 ];
 
 // Two decimals of precision -> 760 = 7.60
-const distro = [1550, 1200, 950, 750, 650, 600, 600, 650, 700, 750, 650, 550, 400];
+const distro = [
+    1550, 1200, 950, 750, 650, 600, 600, 650, 700, 750, 650, 550, 400,
+];
 
 const initTime = 1635007800;
 
@@ -18,20 +26,27 @@ async function main() {
     currentTime = Math.floor(Date.now() / 1000);
     const [signer, ...addrs] = await ethers.getSigners();
     nonce = await signer.getTransactionCount();
-    UnipoolTokenDistributor = await ethers.getContractFactory("UnipoolTokenDistributor");
+    UnipoolTokenDistributor = await ethers.getContractFactory(
+        "UnipoolTokenDistributor",
+    );
     await notifyRewardAmount(pools[0]);
     await notifyRewardAmount(pools[1]);
 }
 
 async function notifyRewardAmount(pool) {
-    const unipoolTokenDistributor = await UnipoolTokenDistributor.attach(pool.address);
+    const unipoolTokenDistributor = await UnipoolTokenDistributor.attach(
+        pool.address,
+    );
     const periodFinish = await unipoolTokenDistributor.periodFinish();
     const duration = await unipoolTokenDistributor.duration();
     // 1 hour of precision
     if (periodFinish < currentTime + 3600) {
         const pos = Math.floor((currentTime - initTime) / duration);
         console.log("pos:", pos);
-        const amount = ethers.utils.parseEther(pool.amount).mul(distro[pos]).div(10000);
+        const amount = ethers.utils
+            .parseEther(pool.amount)
+            .mul(distro[pos])
+            .div(10000);
         console.log(
             "UnipoolTokenDistributor - notifyRewardAmount:",
             pool.address,
@@ -44,7 +59,11 @@ async function notifyRewardAmount(pool) {
         nonce += 1;
         console.log(tx);
     } else {
-        console.log("UnipoolTokenDistributor - notifyRewardAmount:", pool.address, "already set");
+        console.log(
+            "UnipoolTokenDistributor - notifyRewardAmount:",
+            pool.address,
+            "already set",
+        );
     }
 }
 
--- deployments/notifyRewardAmount/xDAI/distributor.js
+++ deployments/notifyRewardAmount/xDAI/distributor.js
@@ -3,13 +3,24 @@ const hre = require("hardhat");
 const { ethers } = hre;
 
 const pools = [
-    { address: "0x491f1Cc76d619061b833287F493136A2D52BB18e", amount: "10000000" }, // UNI (HNY/GIV)
-    { address: "0x00e97BCf2E9A5F6ECF006f89B094255263B16686", amount: "2500000" }, // UNI (ETH/GIV)
-    { address: "0x4358c99abFe7A9983B6c96785b8870b5412C5B4B", amount: "7500000" }, // $GIV
+    {
+        address: "0x491f1Cc76d619061b833287F493136A2D52BB18e",
+        amount: "10000000",
+    }, // UNI (HNY/GIV)
+    {
+        address: "0x00e97BCf2E9A5F6ECF006f89B094255263B16686",
+        amount: "2500000",
+    }, // UNI (ETH/GIV)
+    {
+        address: "0x4358c99abFe7A9983B6c96785b8870b5412C5B4B",
+        amount: "7500000",
+    }, // $GIV
 ];
 
 // Two decimals of precision -> 760 = 7.60
-const distro = [1550, 1200, 950, 750, 650, 600, 600, 650, 700, 750, 650, 550, 400];
+const distro = [
+    1550, 1200, 950, 750, 650, 600, 600, 650, 700, 750, 650, 550, 400,
+];
 
 const initTime = 1635007800;
 
@@ -18,14 +29,18 @@ async function main() {
     currentTime = Math.floor(Date.now() / 1000);
     const [signer, ...addrs] = await ethers.getSigners();
     nonce = await signer.getTransactionCount();
-    UnipoolTokenDistributor = await ethers.getContractFactory("UnipoolTokenDistributor");
+    UnipoolTokenDistributor = await ethers.getContractFactory(
+        "UnipoolTokenDistributor",
+    );
     await notifyRewardAmount(pools[0]);
     await notifyRewardAmount(pools[1]);
     await notifyRewardAmount(pools[2]);
 }
 
 async function notifyRewardAmount(pool) {
-    const unipoolTokenDistributor = await UnipoolTokenDistributor.attach(pool.address);
+    const unipoolTokenDistributor = await UnipoolTokenDistributor.attach(
+        pool.address,
+    );
     const periodFinish = await unipoolTokenDistributor.periodFinish();
     const duration = await unipoolTokenDistributor.duration();
     console.log(periodFinish);
@@ -35,7 +50,10 @@ async function notifyRewardAmount(pool) {
     if (periodFinish < currentTime + 3600) {
         const pos = Math.floor((currentTime - initTime) / duration);
         console.log("pos:", pos);
-        const amount = ethers.utils.parseEther(pool.amount).mul(distro[pos]).div(10000);
+        const amount = ethers.utils
+            .parseEther(pool.amount)
+            .mul(distro[pos])
+            .div(10000);
         console.log(
             "UnipoolTokenDistributor - notifyRewardAmount:",
             pool.address,
@@ -48,7 +66,11 @@ async function notifyRewardAmount(pool) {
         nonce += 1;
         console.log(tx);
     } else {
-        console.log("UnipoolTokenDistributor - notifyRewardAmount:", pool.address, "already set");
+        console.log(
+            "UnipoolTokenDistributor - notifyRewardAmount:",
+            pool.address,
+            "already set",
+        );
     }
 }
 
