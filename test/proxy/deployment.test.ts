9342bab6e798e149b66ac4ba6b9a268dfeead6e8
Pavle Batuta <pavle@batuta.xyz>
2021-12-14 18:48:08 +0100

    Add proxy test pattern for TokenDistro

---
+++ test/proxy/deployment.test.ts
@@ -0,0 +1,100 @@
+import { BigNumber } from "@ethersproject/bignumber";
+import { Contract, ContractFactory } from "@ethersproject/contracts";
+import { expect } from "chai";
+import { ethers, upgrades } from "hardhat";
+import { beforeEach, describe, it } from "mocha";
+import { duration } from "../utils/time";
+
+const { deployProxy, upgradeProxy } = upgrades;
+const { AddressZero } = ethers.constants;
+const { parseEther: toWei } = ethers.utils;
+const { days, years } = duration;
+
+const upgradeText = "OK";
+
+const totalAmount = toWei("200000");
+const startTime = BigNumber.from(0);
+const startToCliff = days(180);
+const startToEnd = years(5);
+const initialPercentage = 500;
+
+const initialAmount = totalAmount.mul(initialPercentage).div(10000);
+const lockedAmount = totalAmount.sub(initialAmount);
+const token = AddressZero;
+const cancellable = false;
+
+const contractsToTest = [
+    {
+        name: "TokenDistro",
+        initParams: [
+            totalAmount,
+            startTime,
+            startToCliff,
+            startToEnd,
+            initialPercentage,
+            token,
+            cancellable,
+        ],
+        upgradeCheckParams: [
+            totalAmount,
+            startTime,
+            startToCliff,
+            startToEnd,
+            initialAmount,
+            lockedAmount,
+            token,
+            cancellable,
+        ],
+    },
+];
+
+describe("Proxy upgradeability tests", () => {
+    contractsToTest.map((contract) => {
+        const { name, initParams, upgradeCheckParams } = contract;
+        return describe(name, () => {
+            let instance: Contract;
+            let factory: ContractFactory;
+            let factoryV2: ContractFactory;
+
+            beforeEach(async () => {
+                factory = await ethers.getContractFactory(name);
+                factoryV2 = await ethers.getContractFactory(`${name}V2`);
+            });
+
+            it("should be deployable", async () => {
+                instance = await deployProxy(factory, initParams);
+            });
+
+            it("should be upgradeable", async () => {
+                instance = instance || (await deployProxy(factory, initParams));
+
+                const upgradedInstance = await upgradeProxy(
+                    instance.address,
+                    factoryV2,
+                );
+
+                expect(upgradedInstance.address).to.be.eq(instance.address);
+
+                expect(upgradedInstance.checkUpgrade).to.not.eq(undefined);
+                const upgradeResult = await upgradedInstance.checkUpgrade();
+
+                if (typeof upgradeResult === "string") {
+                    expect(upgradeResult).to.be.eq(upgradeText);
+                } else {
+                    expect(upgradeResult[0]).to.be.eq(upgradeText);
+
+                    if (
+                        upgradeCheckParams &&
+                        Array.isArray(upgradeCheckParams)
+                    ) {
+                        for (let i = 0; i < upgradeCheckParams.length; i++) {
+                            const expected = upgradeCheckParams[i];
+                            const actual = upgradeResult[i + 1];
+                            expect(expected).to.eq(actual);
+                        }
+                    }
+                }
+            });
+        });
+    });
+});
