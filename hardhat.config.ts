3bec599b65072febdd86e0c76a5551f1982e95cb
Amin Latifi <a.latifi.al@gmail.com>
2021-12-24 11:38:55 +0330

    Changed gasPrice policy for mainnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -116,7 +116,7 @@ const config: HardhatUserConfig = {
         },
         mainnet: {
             url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-            gasPrice: 120000000000,
+            gasPrice: "auto",
             accounts: process.env.PRIVATE_KEY
                 ? [`${process.env.PRIVATE_KEY}`]
                 : [],
