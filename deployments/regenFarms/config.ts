c6f563ef3589ac4602ddd1f65948898e5bceac8c
Amin Latifi <a.latifi.al@gmail.com>
2022-03-31 22:14:30 +0430

    Deployed fox regen farm

--- deployments/regenFarms/config.ts
+++ deployments/regenFarms/config.ts
@@ -4,19 +4,19 @@ import { IRegenConfig } from "../types";
 const config: IRegenConfig = {
     alreadyDeployedTokenDistroAddress: "",
     newTokenDistroParams: {
-        startTime: 1648857600, // Sunday, 20 February 2022 10:00:00
+        startTime: 1649001600, // Sunday, 03 April 2022 16:00:00 GMT
         cliffPeriod: 0,
-        duration: 157_680_000, // 5 years * 365 days * 24 hours * 3600 seconds = 157680000
+        duration: 149_040_000, // 5 years * 365 days * 24 hours * 3600 seconds = 157680000
         initialPercentage: 10_00, // two decimals of precision
-        tokenAddress: "0x6c16216484069C19530a57762AD6630fB678D00E", // TestFOX
-        totalTokens: "1000000000", // 1 Billion
+        tokenAddress: "0x21a42669643f45bc0e086b8fc2ed70c23d67509d", // FOX
+        totalTokens: "1000001337", // 1 Billion
         cancelable: true,
     },
     unipools: {
-        TestFOX_TestHNY: {
-            uniTokenAddress: "0x6a5689435Fb27f235f4cfe05ADDabf8fF173f0Ea", // HoneySwap DRGIV3/TestELK
+        FOX_HNY: {
+            uniTokenAddress: "0x8a0bee989c591142414ad67fb604539d917889df", // HoneySwap DRGIV3/TestELK
             lmDuration: 1_209_600, // 2 weeks * 7 days * 24 hours * 3600 seconds = 1_209_600
-            rewardAmount: "100000", // The value to assign on tokendistro
+            rewardAmount: "327387.1731240768", // The value to assign on tokendistro
         },
     },
 };
