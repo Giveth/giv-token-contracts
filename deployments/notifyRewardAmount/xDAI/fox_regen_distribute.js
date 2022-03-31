d97c07d50ee281e8a8466d000b0e8618f82f7d3e
Amin Latifi <a.latifi.al@gmail.com>
2022-03-31 22:23:26 +0430

    Added fox regen distributor script

---
+++ deployments/notifyRewardAmount/xDAI/fox_regen_distribute.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-use-before-define */
+const hre = require("hardhat");
+const { ethers } = hre;
+
+const pools = [
+    {
+        address: "0x502EC7a040F486EE6Cb7d634D94764874B29dE68",
+        amount: "327387.1731240768",
+    }, // UNI (HNY/GIV)
+];
+
+// Two decimals of precision -> 760 = 7.60
+const distro = [
+    769, 769, 769, 769, 769, 769, 769, 769, 769, 769, 769, 769, 769,
+];
+
+const initTime = 1640361600;
+
+let UnipoolTokenDistributor, currentTime, nonce;
+async function main() {
+    currentTime = Math.floor(Date.now() / 1000);
+    const [signer, ...addrs] = await ethers.getSigners();
+    nonce = await signer.getTransactionCount();
+    UnipoolTokenDistributor = await ethers.getContractFactory(
+        "UnipoolTokenDistributor",
+    );
+    await notifyRewardAmount(pools[0]);
+}
+
+async function notifyRewardAmount(pool) {
+    const unipoolTokenDistributor = await UnipoolTokenDistributor.attach(
+        pool.address,
+    );
+    const periodFinish = await unipoolTokenDistributor.periodFinish();
+    const duration = await unipoolTokenDistributor.duration();
+
+    // 1 hour of precision
+    if (periodFinish < currentTime + 3600) {
+        const pos = Math.floor((currentTime - initTime) / duration);
+        console.log("pos:", pos);
+        if (pos < 0) return;
+        const amount = ethers.utils
+            .parseEther(pool.amount)
+            .mul(distro[pos])
+            .div(10000);
+        console.log(
+            "UnipoolTokenDistributor - notifyRewardAmount:",
+            pool.address,
+            "->",
+            ethers.utils.formatEther(amount.toString()),
+        );
+        const tx = await (
+            await unipoolTokenDistributor.notifyRewardAmount(amount, { nonce })
+        ).wait();
+        nonce += 1;
+        console.log(tx);
+    } else {
+        console.log(
+            "UnipoolTokenDistributor - notifyRewardAmount:",
+            pool.address,
+            "already set",
+        );
+    }
+}
+
+main();
