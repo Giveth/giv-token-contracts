68c07d024ed03bc9e51e75bfa88ada63e251e4be
David Llop <931684+sembrestels@users.noreply.github.com>
2022-04-27 14:05:37 +0200

    Add GIVdelegation contract

---
+++ contracts/Curation/GIVdelegation.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity =0.8.6;
+
+
+import "./GIVpower.sol";
+
+contract GIVdelegation is GIVpower {
+    using SafeMathUpgradeable for uint256;
+
+    mapping (address => address) private _delegateOf;
+    mapping (address => uint256) private _delegatedBalanceOf;
+
+    function initialize(uint256 _initialDate, uint256 _roundDuration, address _tokenManager, address _tokenDistribution, uint256 _duration) initializer public {
+      __GIVpower_init(_initialDate, _roundDuration, _tokenManager, _tokenDistribution, _duration);
+    }
+
+    function lock(uint256 _amount, uint256 _rounds) public override {
+        address delegate = delegateOf(msg.sender);
+        delegateTo(address(0));
+        // we check the amount is lower than the lockable amount in the parent's function
+        super.lock(_amount, _rounds);
+        delegateTo(delegate);
+    }
+
+    function unlock(address[] calldata _locks, uint256 _round) public override {
+        address delegate = delegateOf(msg.sender);
+        delegateTo(address(0));
+        // we check the round has passed in the parent's function
+        super.unlock(_locks, _round);
+        delegateTo(delegate);
+    }
+
+    function delegateTo(address _delegate) public {
+        require (_delegate != msg.sender, "Use zero address to delegate on yourself");
+        address _oldDelegate = _delegateOf[msg.sender];
+        if (_oldDelegate != address(0)) {
+            _delegatedBalanceOf[_oldDelegate] = _delegatedBalanceOf[_oldDelegate].sub(balanceOf(msg.sender));
+        }
+        _delegateOf[msg.sender] = _delegate;
+        if (_delegate != address(0)) {
+            _delegatedBalanceOf[_delegate] = _delegatedBalanceOf[_delegate].add(balanceOf(msg.sender));
+        }
+    }
+
+    function delegateOf(address _account) public returns (address) {
+        return _delegateOf[_account];
+    }
+
+    function delegatedBalanceOf(address _account) public returns (uint256) {
+        if (_delegateOf[_account] != address(0)) { // if an account has a delegate, do not count its balance
+            return _delegatedBalanceOf[_account];
+        } else { // otherwise, count the delegated balance on top of the balance
+            return balanceOf(_account).add(_delegatedBalanceOf[_account]);
+        }
+    }
+}
--- contracts/Curation/GIVpower.sol
+++ contracts/Curation/GIVpower.sol
@@ -10,12 +10,13 @@ contract GIVpower is GardenTokenLock, GIVUnipool {
 
     mapping(address => mapping(uint256 => uint256)) private _powerUntilRound;
 
-    function initialize(uint256 _initialDate, uint256 _roundDuration, address _tokenManager, address _tokenDistribution, uint256 _duration) initializer public {
+    function __GIVpower_init(uint256 _initialDate, uint256 _roundDuration, address _tokenManager, address _tokenDistribution, uint256 _duration) initializer public {
       __GardenTokenLock_init(_initialDate, _roundDuration, _tokenManager);
       __GIVUnipool_init(_tokenDistribution, _duration);
     }
 
-    function lock(uint256 _amount, uint256 _rounds) public override {
+    function lock(uint256 _amount, uint256 _rounds) public virtual override {
+        // we check the amount is lower than the lockable amount in the parent's function
         super.lock(_amount, _rounds);
         uint256 round = currentRound().add(_rounds);
         uint256 powerAmount = _amount.mul(_rounds.add(1));
@@ -23,7 +24,7 @@ contract GIVpower is GardenTokenLock, GIVUnipool {
         super.stake(msg.sender, powerAmount);
     }
 
-    function unlock(address[] calldata _locks, uint256 _round) public override {
+    function unlock(address[] calldata _locks, uint256 _round) public virtual override {
         // we check the round has passed in the parent's function
         super.unlock(_locks, _round);
         for (uint i = 0; i < _locks.length; i++) {
