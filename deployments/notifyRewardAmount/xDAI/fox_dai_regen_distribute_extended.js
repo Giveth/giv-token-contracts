3418a8554ec09085e5965444cbe7a84095506833
Amin Latifi <a.latifi.al@gmail.com>
2023-05-25 19:30:19 +0330

    Added fox dai regen extended reward distribute script

---
+++ deployments/notifyRewardAmount/xDAI/fox_dai_regen_distribute_extended.js
@@ -0,0 +1,76 @@
+/* eslint-disable no-use-before-define */
+const hre = require("hardhat");
+const { sendReportEmail } = require("../../mailService/mailService");
+const { ethers } = hre;
+
+const pools = [
+    {
+        address: "0x9A333AD00868472c0314F76DB8dA305B83890129",
+        amount: "735000",
+    }, // UNI (fox/dai)
+];
+
+// Two decimals of precision -> 760 = 7.60
+const distro = [
+    770, 770, 769, 769, 769, 769, 769, 769, 769, 769, 769, 769, 770,
+];
+
+const initTime = 1685460000; // Date and time (GMT): Tuesday, May 30, 2023 15:20:00
+
+let UnipoolTokenDistributor, currentTime, nonce;
+async function main() {
+    currentTime = Math.floor(Date.now() / 1000);
+    const [signer, ...addrs] = await ethers.getSigners();
+    nonce = await signer.getTransactionCount();
+    UnipoolTokenDistributor = await ethers.getContractFactory(
+        "UnipoolTokenDistributor",
+    );
+    await notifyRewardAmount(pools[0]);
+}
+
+async function notifyRewardAmount(pool) {
+    const unipoolTokenDistributor = await UnipoolTokenDistributor.attach(
+        pool.address,
+    );
+    const periodFinish = await unipoolTokenDistributor.periodFinish();
+    const duration = await unipoolTokenDistributor.duration();
+
+    // 10 minutes of precision
+    if (periodFinish < currentTime + 60 * 10) {
+        const pos = Math.floor((currentTime - initTime) / duration);
+        console.log("pos:", pos);
+        if (pos < 0) return;
+        const amount = ethers.utils
+            .parseEther(pool.amount)
+            .mul(distro[pos])
+            .div(10000);
+        console.log(
+            "UnipoolTokenDistributor - notifyRewardAmount:",
+            pool.address,
+            "->",
+            ethers.utils.formatEther(amount.toString()),
+        );
+        const tx = await (
+            await unipoolTokenDistributor.notifyRewardAmount(amount, { nonce })
+        ).wait();
+        nonce += 1;
+        console.log("tx:", tx);
+        await sendReportEmail({
+            farm: "Fox regen farm",
+            network: "Gnosis",
+            pool: pool.address,
+            round: pos + 1,
+            script: "fox/dai regen farm distribute",
+            transactionHash: tx.transactionHash,
+            amount,
+        });
+    } else {
+        console.log(
+            "UnipoolTokenDistributor - notifyRewardAmount:",
+            pool.address,
+            "already set",
+        );
+    }
+}
+
+main();
